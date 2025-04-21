04911b77cb6d6a6d20572c3519108350d26abaae
Jack <devjack050700@gmail.com>
2025-04-21 16:29:16 +0900

    레포지토리 생성

---
+++ java-basic/src/class1/ClassStart1.java
@@ -0,0 +1,22 @@
+package class1;
+
+public class ClassStart1 {
+
+    public static void main(String[] args) {
+        String student1Name = "학생1";
+        int student1Age = 18;
+        int student1Grade = 90;
+        System.out.println("이름: " + student1Name + "나이: " + student1Age + "성적: " + student1Grade);
+
+        String student2Name = "학생2";
+        int student2Age = 18;
+        int student2Grade = 90;
+        System.out.println("이름: " + student1Name + "나이: " + student1Age + "성적: " + student1Grade);
+
+        String student3Name = "학생3";
+        int student3Age = 18;
+        int student3Grade = 90;
+        System.out.println("이름: " + student1Name + "나이: " + student1Age + "성적: " + student1Grade);
+
+    }
+}
