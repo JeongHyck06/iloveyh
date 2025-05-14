d073decfdad2cd4a3254af5bcdb398697430bff0
Jack <devjack050700@gmail.com>
2025-05-14 17:16:58 +0900

    회원 도메인과 리포지토리 만들기

--- hello-spring/src/main/java/hello/hello_spring/repository/MemoryMemberRepository.java
+++ hello-spring/src/main/java/hello/hello_spring/repository/MemoryMemberRepository.java
@@ -6,6 +6,7 @@ import java.util.*;
 
 public class MemoryMemberRepository implements MemberRepository {
 
+
     private static Map<Long, Member> store = new HashMap<>();
     private static long sequence = 0L;
 
@@ -29,5 +30,9 @@ public class MemoryMemberRepository implements MemberRepository {
     @Override
     public List<Member> findAll() {
         return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear();
     }
 }
